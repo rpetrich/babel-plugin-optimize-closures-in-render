ae6036c14893916f93d6a7b789c7314471df59a6
Ryan Petrich <rpetrich@gmail.com>
2017-10-16 23:29:49 -0400

    Always call bind on the target function and rewrite captures to be passed in as arguments; Support pseudo-_this variable

--- example_out.js
+++ example_out.js
@@ -1,33 +1,50 @@
-var __render_pending,
-    __render_lookup = function () {
-	var map;
+var __render_bind = function () {
+	var map = typeof WeakMap != "undefined" && new WeakMap();
+	return function (target, nodeIndex, func, boundValues) {
+		var targetCache;
 
-	if (typeof WeakMap != "undefined") {
-		map = new WeakMap();
-	}
-
-	return function (target, nodeIndex, expectedValues) {
-		var targetCache = map ? map.get(target) || map.set(target, []) : target.__render_cache || (target.__render_cache = []);
-		var nodeCache = targetCache[nodeIndex];
-
-		if (expectedValues && nodeCache) {
-			for (var i = 0; i < expectedValues.length; i++) {
-				if (nodeCache.v[i] !== expectedValues[i]) {
-					nodeCache = 0;
-					break;
-				}
-			}
+		if (map) {
+			(targetCache = map.get(target)) || map.set(target, targetCache = []);
+		} else {
+			targetCache = target.__render_cache || (target.__render_cache = []);
 		}
 
+		var nodeCache = targetCache[nodeIndex];
+
 		if (nodeCache) {
+			if (boundValues) {
+				for (var i = 0; i < boundValues.length; i++) {
+					if (nodeCache.v[i] !== boundValues[i]) {
+						nodeCache.v = boundValues;
+						return nodeCache._ = func.bind.apply(func, boundValues);
+					}
+				}
+			}
+
 			return nodeCache._;
 		}
 
-		__render_pending = targetState[nodeIndex] = {
-			v: expectedValues
+		var result = func.bind.apply(func, boundValues || [target]);
+		targetCache[nodeIndex] = {
+			v: boundValues,
+			_: result
 		};
+		return result;
 	};
 }();
+
+var _ref = function () {
+	return this.simpleMethod();
+},
+    _ref2 = function (value) {
+	return this.someMethod(value);
+},
+    _ref3 = function (onSubmit, value, anotherValue, event) {
+	return onSubmit(this.props.value, anotherValue, event);
+},
+    _ref4 = function (value) {
+	setTimeout(bar => console.log("focused!", foo, bar, value), 0);
+};
 
 var foo;
 
@@ -38,16 +55,14 @@ class MyComponent {
 		// Kind of silly, but represents all combinations
 		const { value, anotherValue, onSubmit } = this.props;
 
-		const passthrough = __render_lookup(this, 0) || (__render_pending._ = () => this.simpleMethod());
-		const wrappingValue = __render_lookup(this, 1, [value]) || (__render_pending._ = () => this.someMethod(value));
-		const boundFunction = __render_lookup(this, 2, [value, anotherValue]) || (__render_pending._ = this.someMethod.bind(this, value, anotherValue));
+		const passthrough = __render_bind(this, 0, _ref);
+		const wrappingValue = __render_bind(this, 1, _ref2, [this, value]);
+		const boundFunction = __render_bind(this, 2, this.someMethod);
 
 		return React.createElement("button", {
-			onClick: __render_lookup(this, 3, [onSubmit, anotherValue]) || (__render_pending._ = event => onSubmit(this.props.value, anotherValue, event)),
+			onClick: __render_bind(this, 3, _ref3, [this, onSubmit, value, anotherValue]),
 			onBlur: boundFunction,
-			onFocus: __render_lookup(this, 4, [value]) || (__render_pending._ = function () {
-				setTimeout(bar => console.log("focused!", foo, bar, value), 0);
-			})
+			onFocus: __render_bind(this, 4, _ref4, [this, value])
 		});
 	}
 }
--- plugin.js
+++ plugin.js
@@ -1,105 +1,180 @@
+function patchMethod(methodPath, types) {
+	let i = 0;
+	let usedHelpers = 0;
+	function cachePathOnThis(path) {
+		let shouldReplace = true;
+		let dependentValues = [];
+		let dependsOnThis = false;
+		let skipThis = 0;
+		let _thisIndex = -1;
+		// Find dependent values from the render scope
+		path.traverse({
+			FunctionExpression: {
+				enter(expressionPath) {
+					skipThis++;
+				},
+				exit(expressionPath) {
+					skipThis--;
+				}
+			},
+			Identifier(identifierPath) {
+				const name = identifierPath.node.name;
+				const binding = identifierPath.scope.getBinding(name);
+				if (binding) {
+					let bindingPath = binding.path;
+					while (bindingPath && bindingPath != path) {
+						if (bindingPath === methodPath) {
+							if (binding.constant) {
+								// Possibly treat arguments named as _this specially
+								if (name === "_this" && skipThis === 0) {
+									_thisIndex = dependentValues.length;
+								}
+								// Found a constant from the render scope, include it in the cache check
+								dependentValues.push(identifierPath.node);
+							} else {
+								// binding was not constant, do not optimize this
+								shouldReplace = false;
+								identifierPath.stop();
+							}
+							break;
+						}
+						bindingPath = bindingPath.parentPath;
+					}
+				}
+			},
+			ThisExpression(path) {
+				if (skipThis === 0) {
+					// Bind expression or direct child arrow function, using this
+					dependsOnThis = true;
+				} else if (skipThis === 1) {
+					// Direct child normal function, using this
+					shouldReplace = false;
+					path.stop();
+				}
+			}
+		});
+		if (!skipThis && _thisIndex !== -1) {
+			// Rewrite pseudo-_this parameter back into this
+			dependentValues.splice(_thisIndex, 1);
+			dependsOnThis = true;
+			path.traverse({
+				FunctionExpression(path) {
+					path.skip();
+				},
+				Identifier(path) {
+					if (path.node.name === "_this") {
+						path.replaceWith(types.thisExpression());
+					}
+				}
+			});
+		}
+		if (shouldReplace) {
+			usedHelpers = true;
+			const lookupArguments = [types.thisExpression(), types.numericLiteral(i)];
+			if (path.isCallExpression() && path.get("callee").isMemberExpression() && !path.node.callee.computed && path.node.callee.property.name == "bind") {
+				// Replace .bind calls
+				lookupArguments.push(path.node.callee.object);
+				if (path.node.arguments.length && path.node.arguments[0].type !== "ThisExpression") {
+					lookupArguments.push(types.arrayExpression(path.node.arguments));
+				}
+				path.replaceWith(types.callExpression(types.identifier("__render_bind"), lookupArguments));
+			} else if (path.isFunctionExpression() || path.isArrowFunctionExpression()) {
+				// Replace function expressions
+				const id = methodPath.scope.generateUidIdentifierBasedOnNode(path.node);
+				const init = path.node;
+				if (dependentValues.length || dependsOnThis) {
+					lookupArguments.push(id);
+					if (dependentValues.length) {
+						lookupArguments.push(types.arrayExpression([types.thisExpression()].concat(dependentValues)));
+					}
+					path.replaceWith(types.callExpression(types.identifier("__render_bind"), lookupArguments));
+				} else {
+					path.replaceWith(id);
+				}
+				const params = dependentValues.concat(init.params);
+				const body = init.body.type == "BlockStatement" ? init.body : types.blockStatement([types.returnStatement(init.body)]);
+				methodPath.scope.parent.push({ id, init: types.functionExpression(init.id, params, body, init.generator, init.async) });
+			} else {
+				// How did we get here?
+				return;
+			}
+			i++;
+			path.skip();
+		}
+	}
+	methodPath.traverse({
+		// Arrow functions
+		ArrowFunctionExpression: cachePathOnThis,
+		// Functions
+		FunctionExpression(path) {
+			cachePathOnThis(path);
+			path.skip();
+		},
+		// Call expressions
+		CallExpression(path) {
+			const callee = path.get("callee");
+			// But only calls to bind
+			if (callee.isMemberExpression() && !callee.node.computed && callee.node.property.name == "bind") {
+				cachePathOnThis(path);
+			}
+		}
+	});
+	return usedHelpers;
+}
+
 module.exports = function({ types, template }) {
 	return {
 		visitor: {
-			ClassMethod(methodPath) {
-				if (methodPath.node.key.name !== "render") {
-					return;
+			ClassMethod(path) {
+				if (path.node.key.name === "render") {
+					this.usedHelpers |= patchMethod(path, types);
 				}
-				let i = 0;
-				const that = this;
-				function cachePathOnThis(path) {
-					let shouldReplace = true;
-					let dependentValues = [];
-					// Find dependent values from the render scope
-					path.traverse({
-						MemberExpression(path) {
-							path.skip();
-						},
-						Identifier(identifierPath) {
-							const name = identifierPath.node.name;
-							const binding = identifierPath.scope.getBinding(name);
-							if (binding) {
-								let bindingPath = binding.path;
-								while (bindingPath && bindingPath != path) {
-									if (bindingPath === methodPath) {
-										if (binding.constant) {
-											// Found a constant from the render scope, include it in the cache check
-											dependentValues.push(identifierPath.node);
-										} else {
-											// binding was not constant, do not optimize this
-											shouldReplace = false;
-											identifierPath.stop();
-										}
-										break;
-									}
-									bindingPath = bindingPath.parentPath;
-								}
+			},
+			FunctionExpression(path) {
+				const parentPath = path.parentPath;
+				if (parentPath.isAssignmentExpression()) {
+					const leftPath = parentPath.get("left");
+					if (leftPath.isMemberExpression() && !leftPath.node.computed && leftPath.node.property.name == "render") {
+						const objectPath = leftPath.get("object");
+						if (objectPath.isMemberExpression() && !objectPath.node.computed && objectPath.node.property.name == "prototype") {
+							if (patchMethod(path, types)) {
+								this.usedHelpers = true;
 							}
 						}
-					});
-					if (shouldReplace) {
-						that.usedHelpers = true;
-						const lookupArguments = [types.thisExpression(), types.numericLiteral(i)];
-						if (dependentValues.length) {
-							lookupArguments.push(types.arrayExpression(dependentValues));
-						}
-						// Try to find this expression in the cache
-						path.replaceWith(types.logicalExpression(
-							"||",
-							types.callExpression(types.identifier("__render_lookup"), lookupArguments),
-							types.assignmentExpression("=", types.memberExpression(types.identifier("__render_pending"), types.identifier("_")), path.node)
-						));
-						i++;
-						path.skip();
 					}
 				}
-				methodPath.traverse({
-					// Arrow functions
-					ArrowFunctionExpression: cachePathOnThis,
-					// Functions
-					FunctionExpression(path) {
-						cachePathOnThis(path);
-						path.skip();
-					},
-					// Call expressions
-					CallExpression(path) {
-						const callee = path.get("callee");
-						// But only calls to bind
-						if (callee.isMemberExpression() && !callee.node.computed && callee.node.property.name == "bind") {
-							cachePathOnThis(path);
-						}
-					}
-				});
 			},
 			Program: {
 				exit(path) {
 					if (this.usedHelpers) {
 						const body = path.get("body.0");
-						body.insertBefore(template(`var __render_pending, __render_lookup = (function() {
+						body.insertBefore(template(`var __render_bind = (function() {
 							// Try using WeakMaps
-							var map;
-							if (typeof WeakMap != "undefined") {
-								map = new WeakMap();
-							}
-							return function(target, nodeIndex, expectedValues) {
-								var targetCache = map ? (map.get(target) || map.set(target, [])) : (target.__render_cache || (target.__render_cache = []));
+							var map = typeof WeakMap != "undefined" && new WeakMap();
+							return function(target, nodeIndex, func, boundValues) {
+								var targetCache;
+								if (map) {
+									(targetCache = map.get(target)) || (map.set(target, targetCache = []));
+								} else {
+									targetCache = target.__render_cache || (target.__render_cache = []);
+								}
 								// Lookup the cache for the particular node
 								var nodeCache = targetCache[nodeIndex];
 								// Check if all the dependent values match
-								if (expectedValues && nodeCache) {
-									for (var i = 0; i < expectedValues.length; i++) {
-										if (nodeCache.v[i] !== expectedValues[i]) {
-											nodeCache = 0;
-											break;
+								if (nodeCache) {
+									if (boundValues) {
+										for (var i = 0; i < boundValues.length; i++) {
+											if (nodeCache.v[i] !== boundValues[i]) {
+												nodeCache.v = boundValues;
+												return nodeCache._ = func.bind.apply(func, boundValues);
+											}
 										}
 									}
-								}
-								// Return the cached value we found
-								if (nodeCache) {
 									return nodeCache._;
 								}
-								// Prepare a new cache for the node and make it pending
-								__render_pending = targetState[nodeIndex] = { v: expectedValues };
+								var result = func.bind.apply(func, boundValues || [target]);
+								targetCache[nodeIndex] = { v: boundValues, _: result };
+								return result;
 							}
 						})();`)());
 						path.stop();
